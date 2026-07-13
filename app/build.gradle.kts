fbe499d25e6d14106200cf6157cbebabb71d63d5
Obieda <bis.2410423@eia.edu.eg>
2026-07-13 21:58:13 +0300

    fix all bugs in app
    Improving performance and fixing libraries and errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
   implementation(platform(libs.firebase.bom))
   // implementation(libs.accompanist.permissions)
   implementation(libs.androidx.activity.compose)
+  implementation(libs.androidx.fragment)
   // implementation(libs.androidx.camera.camera2)
   // implementation(libs.androidx.camera.core)
   // implementation(libs.androidx.camera.lifecycle)
